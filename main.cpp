f3e19fa1b9f90bdfc9bb27ec9fc6857f00b4c1dd
kingnan-Guo <373734199@qq.com>
2025-01-02 14:34:08 +0800

    同步发送数据客户端

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <stdio.h>
 #include "client.h"
+#include "clientSynchronization.h"
+
 
 int main(int arch, char* argv[])
 {
     printf("ReactorClient star \r\n");
 
-
-    client_main(arch, argv);
+    // client_main(arch, argv);
+    clientSynchronization_main(arch, argv);
     return 0;
 }
---
+++ module/clientSynchronization/clientSynchronization.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <fstream>
+using namespace std;
+
+int clientSynchronization_main(int argc,char *argv[]);
+
+
+
+
